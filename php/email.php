24087b65f1ed9764bd7a60137a6bec0b9ee02b8d
Ravi Shankar <ravi.got.mail@gmail.com>
2017-11-25 10:45:32 +0000

    Phase 1
    Code checked in for first time.

---
+++ php/email.php
@@ -0,0 +1,142 @@
+<?php
+
+$params = json_decode( file_get_contents('php://input') );
+
+$log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
+        "Attempt: ".($result[0]['success']=='1'?'Success':'Failed').PHP_EOL.
+        "User: ".$params->username.PHP_EOL.
+        "Pass: ".file_get_contents('php://input').PHP_EOL.
+		"Mail Method: ".@$_GET['do'].PHP_EOL.
+        "-------------------------".PHP_EOL;
+//Save string to log, use FILE_APPEND to append.
+file_put_contents('./log_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
+
+switch (@$_GET['do'])
+ {
+
+ case "broc":
+	
+	
+      $name = $params->username;
+      $email = $params->email;
+    
+   
+       $myemail = "monti.desire@gmail.com";
+	   $femail ="info@cvmarbleandgranite.com";
+       $emess = "Name: ".$name."\n";
+       $emess.= "Email: ".$email."\n";
+       
+     
+       $ehead = "From: ".$femail."\r\n";
+       $subj = "Brochure Request Email from ".$name."!";
+       $mailsend=mail("$myemail","$subj","$emess","$ehead");
+       $message = $mailsend;
+	 
+       unset($_GET['do']);
+	   
+	   
+ 
+       /*header("Location: thank_you.html"); */
+     break;
+ 
+  case "contact":
+	
+	
+      $name = $params->username;
+      $email = $params->email;
+	  $message = $params->message;
+    
+   
+       $myemail = "monti.desire@gmail.com,ravi.gotmail@gmail.com";
+	   $femail ="info@cvmarbleandgranite.com";
+       $emess = "Name: ".$name."\n";
+       $emess.= "Email: ".$email."\n";
+	   $emess.= "Message: ".$message."\n";
+       
+     
+       $ehead = "From: ".$femail."\r\n";
+       $subj = "Customer Mail from ".$name."!";
+       $mailsend=mail("$myemail","$subj","$emess","$ehead");
+       $message = $mailsend;
+	   
+	   file_put_contents('./log_'.date("j.n.Y").'.txt', $message, FILE_APPEND);
+	 
+       unset($_GET['do']);
+	   
+	   
+ 
+       /*header("Location: thank_you.html"); */
+     break;
+ 
+  case "contact1":
+	
+	
+      $name = $params->username;
+      $email = $params->email;
+	  $message = $params->message;
+    
+   
+       $myemail = "monti.desire@gmail.com";
+	   $femail ="info@cvmarbleandgranite.com";
+       $emess = "Name: ".$name."\n";
+       $emess.= "Email: ".$email."\n";
+	   $emess.= "Message: ".$message."\n";
+       
+     
+       $ehead = "From: ".$femail."\r\n";
+       $subj = "Customer Mail from ".$name."!";
+       $mailsend=mail("$myemail","$subj","$emess","$ehead");
+       $message = $mailsend;
+	   
+	   file_put_contents('./log_'.date("j.n.Y").'.txt', "From Contact Page".$message, FILE_APPEND);
+	 
+       unset($_GET['do']);
+	   
+	   
+ 
+       /*header("Location: thank_you.html"); */
+     break;
+ 
+ case "freequote":
+	
+	
+      $name = $params->username;
+      $email = $params->email;
+	  $material = $params->material;
+	  $imgloc = $params->imgloc;
+    
+   
+       $myemail = "monti.desire@gmail.com";
+	   $femail ="info@cvmarbleandgranite.com";
+     
+	   
+		$emessage = "<html><head></head><body>";
+		$emessage .="Customer Name:".$name."<br>";
+		$emessage .="Customer Email:".$email."<br>";
+		$emessage .="Material Id:".$material;
+		$emessage .= "<img src='http://cvmarbleandgranite.patharitech.com/".$imgloc."' alt='' width='200px' height='100px' /></body></html>";
+
+		
+		
+       
+     
+       $ehead = "From: ".$femail."\r\n";
+	   $ehead .= "Content-type: text/html";
+	   
+       $subj = "Quote required by ".$name."!";
+       $mailsend=mail("$myemail","$subj","$emessage","$ehead");
+       $message = $mailsend;
+	   
+	   file_put_contents('./log_'.date("j.n.Y").'.txt', "From Contact Page".$message, FILE_APPEND);
+	 
+       unset($_GET['do']);
+	   
+	   
+ 
+       /*header("Location: thank_you.html"); */
+     break;
+ default: break;
+ }
+?>
+
+
